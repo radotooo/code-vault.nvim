c0ec4f6611cce59dc46cdec8bd433a5cfc9d7757
Radislav Danev <radotooo@gmail.com>
2023-10-16 23:02:40 +0300

    fix: cleanup and some error handling

--- lua/reference/init.lua
+++ lua/reference/init.lua
@@ -1,79 +1,90 @@
 local M = {}
 
--- local table = { one = 'test2 text3 texst 4' }
+local errors = {
+  FILE_NOT_FOUND = "File does not exist!",
+  KEY_EXIST = "Key exist!",
+  NO_DATA = "No data to write.",
+  KEY_NOT_FOUND = "Key does not exist!",
+  FAILED_TO_CREATE = "Failed to create file!"
+}
 
+local function _parse_text(key, text)
+  local file = io.open("output.txt", "r")
+  local data = {}
 
-local function write_to_file(key, text)
-  local file_read = io.open("output.txt", "r+")
-  local table = {}
+  if file then
+    local file_content = file:read("*a")
+    file:close()
 
-  if file_read then
-    local file_content = file_read:read("*a")
-    file_read:close()
-
-    local file_write = io.open("output.txt", "w")
-
-    if file_content and file_content[key] == nil then
-      local current_content = vim.fn.json_decode(file_content)
-      current_content[key] = text
-      table = current_content
-    else
-      if table[key] == nil then
-        print('setva', key, text)
-        table[key] = text
+    if file_content and file_content ~= '' then
+      if file_content[key] == nil then
+        local current_content = vim.fn.json_decode(file_content)
+        current_content[key] = text
+        data = current_content
+      else
+        print(errors.KEY_EXIST)
       end
+    else
+      data[key] = text
+    end
+  end
+
+  return data;
+end
+
+local function _write_to_file(key, text)
+  local data = _parse_text(key, text)
+
+  local file = io.open("output.txt", "w")
+
+  if file then
+    if next(data) ~= nil then
+      local json = vim.fn.json_encode(data)
+      file:write(json)
+    else
+      print(errors.NO_DATA)
     end
 
-    local json = vim.fn.json_encode(table)
-    print(vim.inspect(json), 'json')
-
-    file_write:write(json);
-    file_write:close();
-
-    -- Close the file when you're done
-    print("Data has been written to the file.")
+    file:close();
   else
-    print("Failed does not exist.")
+    print(errors.FAILED_TO_CREATE)
   end
 end
 
-M.get_lines = function(key)
+M.save_reference = function()
   local vstart = vim.fn.getpos("'<")
   local vend = vim.fn.getpos("'>")
 
   local line_start = vstart[2]
   local line_end = vend[2]
 
-  -- or use api.nvim_buf_get_lines
   local lines = vim.fn.getline(line_start, line_end)
 
-  write_to_file(key, lines)
+  vim.ui.input({ prompt = 'Enter reference key: ' }, function(input)
+    if input then
+      _write_to_file(input, lines)
+    end
+  end)
 end
 
-M.set_lines = function(key)
+M.get_reference = function(key)
   local file = io.open("output.txt", "r")
 
   if file then
-    -- Read the entire file content into a string
     local file_content = file:read("*a")
     local table = vim.fn.json_decode(file_content)
 
-    -- Close the file
     file:close()
-
+    -- TODO use telescope for this
     if table[key] then
       local cursor_pos = vim.api.nvim_win_get_cursor(0)[1]
-      print(cursor_pos, 'cursor_pos')
-      vim.api.nvim_buf_set_text(0, cursor_pos, 0, 0, 0, table[key])
+      vim.api.nvim_buf_set_text(0, cursor_pos - 1, 0, cursor_pos - 1, 0, table[key])
+    else
+      print(errors.KEY_NOT_FOUND)
     end
-    -- Print or use the content
-    print("File content:")
-    print(file_content)
   else
-    print("Failed to open the file for reading.")
+    print(errors.FILE_NOT_FOUND)
   end
 end
 
--- M.set_lines({ 'console', 'console', 'console' })
--- print(vim.inspect(table.concat({ 'console', 'console' }, '@'))) -- todo read from file
 return M
